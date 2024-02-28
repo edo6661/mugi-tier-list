09af9a18df618022f2d5b9d5293386542f02d0c2
Edo <1122140027@global.ac.id>
2024-02-28 21:57:44 +0700

    add env fuckkkkk i hate fucking webhooks

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddUser from "./user/AddUser";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 const Header = () => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  });
   return (
     <header className="container">
       <nav className="fl-itc justify-between">
@@ -14,6 +19,12 @@ const Header = () => {
         <Link href="/login" className="">
           Login
         </Link>
+        {isClient && (
+          <>
+            <UserButton />
+            <Link href="/sign-in">Login Clerk</Link>
+          </>
+        )}
         <AddUser />
       </nav>
     </header>
